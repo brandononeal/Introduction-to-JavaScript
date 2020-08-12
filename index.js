3dfa01e9425cb46210e9c20d26c2646a8e403344
Brandon O'Neal <baoneal95@gmail.com>
2020-08-11 17:18:17 -0700

    still working

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
-
+// let votingAge = 21
+// if (votingAge >= 18) {
+//     console.log(true);
+// }
+// COMPLETE
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 
+// Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
-
-//Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
-
-
-
+// console.log(Number('1999'))
+// COMPLETE
 
 //Task d: Write a function to multiply a*b 
 
-
-
-
+// function multiply(a,b) {
+//     console.log(a*b);
+// }
+// multiply(3,7)
+// COMPLETE
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
-
+// function dogYears(age) {
+//     console.log(7*age);
+// }
+// dogYears(25);
+// COMPLETE
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
 //takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 
-//feeding requirements
-// adult dogs at least 1 year 
+//feeding requirements //
+// adult dogs at least 1 year //
 // up to 5 lbs - 5% of their body weight
 // 6 - 10 lbs - 4% of their body weight 
 // 11 - 15 lbs - 3% of their body weight 
 // > 15lbs - 2% of their body weight 
 
-// Puppies less than 1 year
+// Puppies less than 1 year //
 // 2 - 4 months 10% of their body weight
 // 4 - 7 months 5% of their body weight 
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
 
+// function dogFeeder(weight, age){
+//     if (weight <= 5 && age >= 1){
+//         console.log(weight*.05);
+//     }
+//     else if (weight <= 10 && weight >= 6 && age >= 1){
+//         console.log(weight*.04);
+//     }
+//     else if (weight <= 15 && weight >= 11 && age >= 1){
+//         console.log(weight*.03);
+//     }
+//     else if (weight > 15 && age >= 1){
+//         console.log(weight*.02);
+//     }
+//     else if (age >= .16 && age <= .33){
+//         console.log(weight*.10);
+//     }
+//     else if (age >= .34 && age <= .58){
+//         console.log(weight*.5);
+//     }
+//     else if (age >= .59 && age <= 1){
+//         console.log(weight*.4);
+//     }
+//     else {
+//         console.log('Invalid parameters');
+//     }
+// }
 
-
+// dogFeeder(15,1)
+// COMPLETE
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -81,8 +109,6 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
-
 
 
 
@@ -97,7 +123,7 @@
   
 
   
-  
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
